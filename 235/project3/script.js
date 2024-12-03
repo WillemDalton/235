c3cb978594415f28ed7db1944f95a6a476b19fb5
Willem Dalton <whd1964@rit.edu>
2024-12-03 18:15:30 -0500

    project 3

--- 235/project3/script.js
+++ 235/project3/script.js
@@ -1,39 +1,56 @@
 const canvas = document.querySelector(".garden");
-const context = canvas.getContext("2d");
+const svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
+canvas.appendChild(svg);
 
 let isDrawing = false;
 
 let storedX = 0;
 let storedY = 0;
 
+
 // when the mouse is down, we wanna draw.
-document.addEventListener("mousedown", () =>
+document.addEventListener("mousedown", (event) =>
 {
+    storedX = event.clientX;
+    storedY = event.clientY;
+
     isDrawing = true;
 });
 
-document.addEventListener("mouseup", () => {
+document.addEventListener("mouseup", (event) => {
+
+    storedX = event.clientX;
+    storedY = event.clientY;
+
     isDrawing = false;
 })
 
-canvas.addEventListener("mousemove", (event) => {
-    const x = event.clientX;
-    const y = event.clientY;
+document.addEventListener("mousemove", (event) => {
 
+    const rect = canvas.getBoundingClientRect();      
     if(isDrawing)
     {
-        context.beginPath();
-
-        context.moveTo(0,0);
-
-        console.log(x);
-        context.lineTo(x,y);
-
-        context.stroke();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        
+        // if the distance between line is greater than 5px, we wanna draw!
+        if(Math.sqrt(Math.pow((x - storedX), 2) + Math.pow((y - storedY), 2)) > 15)
+        {
+            createLine(storedX, storedY, x, y);
+            storedX = x;
+            storedY = y;
+        }
     }
-
-    setInterval(() => {
-        storedX = x;
-        storedY = y;
-    }, 10);
 })
+
+
+const createLine = (startPosX, startPosY, endPosX, endPosY) =>
+{
+    let line = document.createElementNS('http://www.w3.org/2000/svg','line');
+    line.setAttribute("x1", startPosX);
+    line.setAttribute("y1", startPosY);
+    line.setAttribute("x2", endPosX);
+    line.setAttribute("y2", endPosY);
+
+    svg.appendChild(line);
+}
